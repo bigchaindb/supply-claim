35bad861ac1c3b5210ebd43588396c39be520d6a
manolodewiner <manolosingular@gmail.com>
2017-10-12 10:41:37 +0200

    Get wallet added

--- backend/epcis_api/xtech_utils.py
+++ backend/epcis_api/xtech_utils.py
@@ -4,6 +4,7 @@ import uuid
 import conf
 
 ENDPOINT = conf.XTECH_END
+GET_WALLET = conf.XTECH_END + conf.XTECH_GETW
 TRANS = conf.XTECH_END + conf.XTECH_TRANS
 DEF_WALLET = conf.XTECH_DEFW
 
@@ -13,8 +14,6 @@ def add_wallet(public_key):
     payload = {'user_id': public_key}
     r = requests.post(add_wallet_url, payload)
     response = r.json()
-    #in case there is an error
-    print('Error adding wallet: ' + str(response['error']))
     resp_data = ''
     try:
         resp_data = response['data'][0]
@@ -22,11 +21,28 @@ def add_wallet(public_key):
         print('Error: Wallet not created. Possible wallet id duplicated.')
         return 0
     
-    
     print('----Top up----')
     top_up(resp_data['uuid'], 2200)
     print('Wallet UUID: ' + resp_data['uuid'])
     return resp_data['uuid']
+
+
+def get_wallet(wallet_uuid):
+    payload = {'uuid': wallet_uuid}
+    r = requests.post(GET_WALLET, payload)
+    response = r.json()
+    resp_data = ''
+    try:
+        resp_data = response['data'][0]
+    except IndexError:
+        print('Error: Could not get wallet.')
+        return 0
+    print('Wallet UUIID '+resp_data['uuid']+ ', Balance: ' + str(resp_data['total_balance']))
+    return {
+        'wallet_uuid':resp_data['uuid'],
+        'balance':resp_data['total_balance'],
+        'user_id':resp_data['user_id']
+    }
 
 
 def transfer(wallet_uuid, amount, reference):
--- backend/test_wallet.py
+++ backend/test_wallet.py
@@ -3,11 +3,13 @@ import uuid
 
 
 #Create a wallet and top up. Unique id generated
-wallet_id = xtech_utils.add_wallet(uuid.uuid4())
+wallet_uuid = xtech_utils.add_wallet(uuid.uuid4())
+
+print('----GET_WALLET----')
+xtech_utils.get_wallet(wallet_uuid)
 
 amount = 800 #any number to test the transfer
-
-if(wallet_id != 0):
+if(wallet_uuid != 0):
     print('----Transfer----')
-    xtech_utils.transfer(wallet_id, amount,'Transfer test' )    
+    xtech_utils.transfer(wallet_uuid, amount,'Transfer test' )    
     
